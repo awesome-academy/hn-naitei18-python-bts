b2c75d2a8dffb39c710b94dfcd35f6807b54114a
dangthihoa <dang.thi.hoa@sun-asterisk.com>
2020-09-14 16:52:15 +0700

    Create Booking View Test

--- travel/tests/test_views.py
+++ travel/tests/test_views.py
@@ -27,3 +27,38 @@ class ReviewListViewTest(TestCase):
 
     def test_pagination(self):
         pass
+
+class CreateBookingViewTest(TestCase):
+    def setUp(self):
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK',is_active = True)
+
+        test_user1.save()
+
+        tour = Tour.objects.create(title="HaNoi", destination="a", start_location="b", date=4, content="content",
+                            place="place", base_price=400, rating=5)
+        tour.save()
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get('/tour/1/booking')
+        self.assertRedirects(response, '/accounts/login/?next=/tour/1/booking')
+
+    def test_logged_in_uses_correct_template(self):
+        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = self.client.get('/tour/1/booking')
+        # Check our user is logged in
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        # Check that we got a response "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check we used correct template
+        self.assertTemplateUsed(response, 'travel/create_booking.html')
+
+    def test_creating_vote(self):
+        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        tour = Tour.objects.get(id=1)
+        url = reverse('booking', args=[tour.id])
+        response = self.client.post(url,{'start_date': ['2020-09-19'], 'members': ['1']})
+        self.assertEqual(response.status_code,302)
+
+
+
